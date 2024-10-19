a60e02292967a753f26532d6b65b0232cd13fa98
Liam Wylie <liamswork@yahoo.co.uk>
2024-10-19 18:12:41 +0100

    Creating a basic responsive header with some starting links.

--- resources/js/Layouts/Header.vue
+++ resources/js/Layouts/Header.vue
@@ -5,24 +5,36 @@ defineProps({
 </script>
 
 <template>
-    <header class="grid grid-cols-7 text-white py-4">
-        <div class="col-span-1 flex flex-col lg:items-center lg:justify-center">
-            <h2 class="text-white uppercase">Liam Wylie</h2>
-        </div>
-        <div class="col-span-6 flex flex-row lg:items-center lg:justify-evenly">
-            <a>Test LINK!!!!</a>
-            <a>Test Link</a>
-            <a>Test Link</a>
-            <a>Test Link</a>
-            <a href="https://github.com/liamswork" target="_blank">
-                <i class="fa-brands fa-github text-2xl"></i>
-            </a>
-            <a href="https://www.linkedin.com/in/liam-wylie/" target="_blank">
-                <i class="fa-brands fa-linkedin text-2xl"></i>
-            </a>
-            <a href="https://discord.com/users/174583425355874305" target="_blank">
-                <i class="fa-brands fa-discord text-2xl"></i>
-            </a>
+    <header>
+        <div class="container mx-auto flex flex-col lg:flex-row items-center justify-between p-4">
+            <!-- Site Title / Logo -->
+            <div class="mb-4 lg:mb-0">
+                <h1 class="text-2xl font-bold uppercase">Liam Wylie</h1>
+            </div>
+
+            <!-- Navigation Links -->
+            <nav>
+                <ul class="flex flex-row lg:flex-row items-center gap-8 text-center">
+                    <li><a href="#" class="transform inline-block h-8 transition duration-500 hover:scale-125">CVV</a></li>
+                    <li><a href="#" class="transform inline-block h-8 transition duration-500 hover:scale-125">About Me</a></li>
+                    <li>
+                        <a href="https://github.com/liamswork" target="_blank">
+                            <i class="fa-brands fa-github text-2xl transform h-8 transition duration-500 hover:scale-125"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/liam-wylie/" target="_blank">
+                            <i class="fa-brands fa-linkedin text-2xl transform h-8 transition duration-500 hover:scale-125"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://discord.com/users/174583425355874305" target="_blank">
+                            <i class="fa-brands fa-discord text-2xl transform h-8 transition duration-500 hover:scale-125"></i>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     </header>
+
 </template>
